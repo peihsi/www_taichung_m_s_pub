32dd916680ad633f4813bcafc3c4a903026be030
Peterchen810 <peterchen810@gmail.com>
2016-12-22 16:00:22 +0800

    流程圖

--- application/views/onepage.php
+++ application/views/onepage.php
@@ -61,6 +61,10 @@
 			    width: 450px;
 			    top: -3em;
 			}
+			.popover{
+    			max-width: 300px; /* Max Width of the popover (depending on the container!) */
+    			white-space: pre-wrap;  
+			}
 		</style>
 	</head>
 	<body data-spy="scroll" data-target="#scrollTarget" data-offset="150" id="fullScreen">
@@ -91,7 +95,7 @@
 									<li><a href="#team"><i class="icon-users"></i>線上試算</a> </li>
 									<li style="display: none;"><a href="#contactSlice" id="homeform-nav-bt"><i class="icon-mail"></i>Contact</a> </li>
 									<li><a href="#process"><i class="icon-users"></i>進度查詢</a> </li>
-									<li><a href="https://wrapbootstrap.com/theme/ashley-one-page-parallax-bootstrap-3-WB0R11207?ref=littleNeko" target="_blank"><i class="icon-download-outline"></i>Download</a> </li>
+									<!--<li><a href="https://wrapbootstrap.com/theme/ashley-one-page-parallax-bootstrap-3-WB0R11207?ref=littleNeko" target="_blank"><i class="icon-download-outline"></i>Download</a> </li>-->
 								</ul>
 							</div>
 
@@ -138,7 +142,7 @@
 						<div class="row">
 							<div class="col-sm-4"> <img src="/One_page/images/theme-pics/talk.jpg" alt="services" class="img-responsive"> </div>
 							<div class="col-sm-4">
-								<h2>法規精神</h2>
+								<h2 >法規精神</h2>
 
 								<p>依法服役是每個役男應盡的國民義務。但為了避免部分經濟弱勢家庭因役男入伍服役而使家庭經濟陷入拮据困頓，政府特別制定了服兵役役男家屬生活扶助規定
 								</p>
@@ -176,7 +180,7 @@
 								</div>		
 							</div>
 							<div class="col-md-4 hidden-xs">
-								<img src="/One_page/images/theme-pics/confert.png" alt="Ashley, Bootstrap website template" class="img-responsive">
+								<img src="/One_page/images/theme-pics/confert2.png" alt="Ashley, Bootstrap website template" class="img-responsive">
 							</div>
 						</div>
 					</div>
@@ -233,6 +237,42 @@
 								</a>
 							</div>	
 
+						</div>
+						<hr>
+						<div class="row">
+							<!--<div class="col-md-12 col-sm-12" style="text-align: center;">
+							<br><br><br>
+								<img style="zoom: 0.5;" id="Image-Maps-Com-image-maps-2016-12-21-231457" src="http://www.image-maps.com/m/private/0/95i2qn9uoq3kucgfds7ivnu3s6_ilovepdf_com-1.png" border="0" width="2043" height="2301" orgWidth="2043" orgHeight="2301" usemap="#image-maps-2016-12-21-231457" alt="" />
+<map name="image-maps-2016-12-21-231457" id="ImageMapsCom-image-maps-2016-12-21-231457">
+<area id="M1" alt="" title="" shape="rect" coords="430,464,849,625" style="outline:none;" target="_self"  data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?"    />
+<area id="M3" alt="" title="" href="http://www.image-maps.com/" shape="rect" coords="468,1153,847,1383" style="outline:none;" target="_self"     />
+<area id="M4" alt="" title="" href="http://www.image-maps.com/" shape="rect" coords="542,1541,772,1659" style="outline:none;" target="_self"     />
+<area id="M5" alt="" title="" href="http://www.image-maps.com/" shape="rect" coords="466,1767,848,1948" style="outline:none;" target="_self"     />
+<area id="M6" alt="" title="" href="http://www.image-maps.com/" shape="rect" coords="390,2045,921,2295" style="outline:none;" target="_self"     />
+<area id="M2.1" alt="" title="" href="http://www.image-maps.com/" shape="rect" coords="954,855,1333,1047" style="outline:none;" target="_self"     />
+<area id="M2.3" alt="" title="" href="http://www.image-maps.com/" shape="rect" coords="1405,1233,1860,1425" style="outline:none;" target="_self"     />
+<area shape="rect" coords="2041,2299,2043,2301" alt="Image Map" style="outline:none;" title="Image Map" href="http://www.image-maps.com/index.php?aff=mapped_users_0" />
+<area id="M2" alt="" title="" href="http://www.image-maps.com/" shape="poly" coords="655,862,472,953,657,1046,842,954" style="outline:none;" target="_self"     />
+<area id="M2.2" alt="" title="" href="http://www.image-maps.com/" shape="poly" coords="1613,825,1408,973,1615,1123,1819,973" style="outline:none;" target="_self"     />
+</map>
+							</div>-->
+							<br><br><br>
+							<div class="col-md-12 col-sm-12" style="text-align: center;">
+								<img style="width: 100%" id="Image-Maps-Com-image-maps-2016-12-21-231457" src="/One_page/images/ilovepdf_com (white).png">
+								<div style="position: absolute;top: 20%;left: 20%;width: 23%;height: 7.5%;/*background-color: rgba(178, 78, 78, 0.59);*/"   data-toggle="popover" title="需準備的文件" data-content="戶口名簿/戶籍謄本
+財產總歸戶清單
+全戶所得清單
+學生證/在學證明/錄取通知
+診斷證明/身心障礙證明/重大傷病證明" ></div>
+								<div style="position: absolute;top: 37%;left: 22%;width: 21%;height: 9%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="由承辦人初步審核" data-original-title="初審階段"></div>
+								<div style="position: absolute;top: 37%;left: 48.2%;width: 21%;height: 9%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="若發現資料不齊，承辦人將民眾通知補件" data-original-title="通知補件"></div>
+								<div style="position: absolute;top: 35.5%;left: 72.2%;width: 23%;height: 13.5%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="所有文件與資訊將被檢查驗證與計算" data-original-title="補件審查"></div>
+								<div style="position: absolute;top: 53.5%;left: 72.2%;width: 23%;height: 8.5%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="若不符資格，將通知民眾申請駁回" data-original-title="駁回申請"></div>
+								<div style="position: absolute;top: 49.5%;left: 22%;width: 21%;height: 11%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="經過區公所承辦人、業務主管、區長審核通過之案件，將再向上提報給民政局" data-original-title="陳報民政局"></div>
+								<div style="position: absolute;top: 66.5%;left: 26%;width: 13%;height: 6%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="線上電子收文" data-original-title="收件"></div>
+								<div style="position: absolute;top: 76.5%;left: 22%;width: 21%;height: 8.5%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="民政局複審區公所提交之民眾申請案與相關附件" data-original-title="審查資料"></div>
+								<div style="position: absolute;top: 88.5%;left: 18%;width: 29%;height: 11.5%;/*background-color: rgba(178, 78, 78, 0.59);*/" data-toggle="popover" title="" data-content="將最後複審結果通知民眾" data-original-title="函覆申請人"></div>
+							</div>
 						</div>
 					</div>
 				</section>
@@ -504,8 +544,8 @@
 						<div class="maskParent">
 							<div class="paralaxMask"></div>
 							<div class="paralaxText">
-								<i class="icon-star iconBig"></i>
-								<blockquote class="mt15">Write drunk; edit sober.<br/><small>ERNEST HEMINGWAY </small></blockquote>
+								<!--<i class="icon-star iconBig"></i>-->
+								<br/><br/><br/>
 							</div>
 						</div>
 					</div>
@@ -906,5 +946,10 @@
 		<!-- Custom  -->
 		<script type="text/javascript" src="/One_page/js/custom.js"></script>
 		<script type="text/javascript" src="/One_page/js/home_count.js"></script>
+		<script type="text/javascript">
+			$(function () {
+				  $('[data-toggle="popover"]').popover({ trigger: "hover" , container: 'body'})
+			})
+		</script>
 	</body>
 	</html>
